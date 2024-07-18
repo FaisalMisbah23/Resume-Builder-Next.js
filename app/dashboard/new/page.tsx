9faecc8a03d5e2e418681de6d70c841aa2420a81
Faisal Misbah <132295428+FaisalMisbah23@users.noreply.github.com>
2024-07-18 06:45:00 +0500

    fix error & add image

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -11,7 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
-import prisma from "@/app/lib/db";
+import prisma from "@/app/lib/mongodb";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import { unstable_noStore as noStore } from "next/cache";
